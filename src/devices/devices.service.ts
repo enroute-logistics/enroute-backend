c68d2dc5735679928106400ee579113fe75e81f5
Henok Seifu <henok.seifu@cozero.io>
2025-10-24 22:20:14 +0300

    fix: Change default days to return past locations for

--- src/devices/devices.service.ts
+++ src/devices/devices.service.ts
@@ -4,7 +4,7 @@ import Device from '../interfaces/device.interface'
 import Position from '../interfaces/position.interface'
 import { MapboxService } from '../common/services/mapbox.service'
 import { convertKnotsToKmH, normalizePositionTimestamps } from '../common/utils'
-const DEFAULT_DAYS_AGO = 10
+const DEFAULT_DAYS_AGO = 1
 
 @Injectable()
 export class DevicesService {
